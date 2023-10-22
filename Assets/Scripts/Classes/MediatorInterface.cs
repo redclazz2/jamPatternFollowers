f64304965f756e19d88211ffb6955f4b4374cde3
JDKdev <jose.cabrera.2021@upb.edu.co>
2023-10-22 17:04:42 -0500

    restart all classes

--- Assets/Scripts/Classes/MediatorInterface.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface MediatorInterface
-{
-    public void notify(GameComponent sender, string _event);
-	public void notify(UIManager uiManager, string _event);
-}
